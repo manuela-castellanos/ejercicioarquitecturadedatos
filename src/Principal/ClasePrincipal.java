ba70152818d9b115ec4f4296524821557f83dcb5
PERSONAL <manuela.castellanos@uniminuto.edu.co>
2023-02-09 21:03:21 -0500

    agregar linea

--- src/Principal/ClasePrincipal.java
+++ src/Principal/ClasePrincipal.java
@@ -5,6 +5,10 @@ public class ClasePrincipal {
     public static void main(String[] args) {
 
         System.out.println("Hola Clase");
+        System.out.println("Mi primer rama");
+        System.out.println("Estamos aprendiendo GIT");
+
+
 
 
     }
